3d2d50c3ef0f298e757e1cc522fdb671e637df92
natalia <natka545@mail.ru>
2022-10-04 19:59:13 +0300

    Решили задачу 6

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// программа, которая принимает на вход принимает число и выдает, является ли число чётным
+
+Console.Clear();
+Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine()!);
+
+if ((a % 2) > 0)
+{
+    Console.WriteLine("нет, число не чётное");
+}
+else 
+{
+    Console.WriteLine("да, число чётное");
+}
